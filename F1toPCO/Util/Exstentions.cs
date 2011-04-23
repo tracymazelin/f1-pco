ff28723272ee68ac3a6ac24f670beea1bac09e0f
kleinke49 <kleinke49@gmail.com>
2011-04-23 16:07:32 -0500

    Adding exstention methods

---
+++ F1toPCO/Util/Exstentions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace F1toPCO.Util {
+    public static class Exstentions {
+        public static string ToPublicUrl(this UrlHelper urlHelper, Uri relativeUri) {
+
+            var httpContext = urlHelper.RequestContext.HttpContext;
+
+            var uriBuilder = new UriBuilder {
+                Host = httpContext.Request.Url.Host,
+                Path = "/",
+                Port = 80,
+                Scheme = "http",
+            };
+
+            if (httpContext.Request.IsLocal) {
+                uriBuilder.Path = "/F1toPCO/";
+                uriBuilder.Port = httpContext.Request.Url.Port;
+            }
+
+            return new Uri(uriBuilder.Uri, relativeUri).AbsoluteUri;
+        }
+    }
+}
