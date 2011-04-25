cd134528b9e6de1c52e191af57853d764007ec8b
kleinke49 <kleinke49@gmail.com>
2011-04-25 00:36:27 -0500

    Updated to correct key.  Changed URL's to staging.

--- F1toPCO/Util/PrivateConsts.cs
+++ F1toPCO/Util/PrivateConsts.cs
@@ -6,12 +6,12 @@ using System.Web;
 namespace F1toPCO.Util {
     public class PrivateConsts {
         //Staging
-        //public const string f1ConsumerKey ="163";
-        //public const string f1ConsumerSecret = "de1bee74-93c1-4a72-b6e5-0192e5569219";
+        public const string f1ConsumerKey = "257";
+        public const string f1ConsumerSecret = "d514c4c5-8374-49f0-8d25-35e379adb37f";
 
         //Prod
-        public const string f1ConsumerKey = "34";
-        public const string f1ConsumerSecret = "85eb8861-7b57-44a3-9d53-d023003e6273"; 
+        //public const string f1ConsumerKey = "34";
+        //public const string f1ConsumerSecret = "85eb8861-7b57-44a3-9d53-d023003e6273"; 
    
         public const string pcoConsumerKey  ="HfK94IoIKmm40sHeVykg";
         public const string pcoConsumerSecret = "wBWSl0szv2PhuGSxBUf7xyUjnnW389Bzou6EgPFA";
--- F1toPCO/Util/Util.cs
+++ F1toPCO/Util/Util.cs
@@ -12,12 +12,12 @@ namespace F1toPCO.Util
 {
     public static class URL {
         //Staging
-        //public const string f1BaseUrl = "https://{0}.staging.fellowshiponeapi.com";
-        //public const string f1AuthorizeUrl = "https://{0}.staging.fellowshiponeapi.com/v1/PortalUser/Login?oauth_token={1}&oauth_callback={2}";
+        public const string f1BaseUrl = "https://{0}.staging.fellowshiponeapi.com";
+        public const string f1AuthorizeUrl = "https://{0}.staging.fellowshiponeapi.com/v1/PortalUser/Login?oauth_token={1}&oauth_callback={2}";
         
         //Prod
-        public const string f1BaseUrl = "https://{0}.fellowshiponeapi.com";
-        public const string f1AuthorizeUrl = "https://{0}.fellowshiponeapi.com/v1/PortalUser/Login?oauth_token={1}&oauth_callback={2}";
+        //public const string f1BaseUrl = "https://{0}.fellowshiponeapi.com";
+        //public const string f1AuthorizeUrl = "https://{0}.fellowshiponeapi.com/v1/PortalUser/Login?oauth_token={1}&oauth_callback={2}";
         public const string f1CalBack = "CallBack?provider=f1";
         
         public const string pcoBaseUrl = "https://www.planningcenteronline.com";
