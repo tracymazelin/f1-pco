6c6b32778f677d66d585d5175408bc5b71126711
kleinke49 <kleinke49@gmail.com>
2012-02-21 11:25:48 -0600

    Adding LICENSE and README

---
+++ Scripts/DBScript.sql
@@ -0,0 +1,295 @@
+
+/****** Object:  Table [dbo].[ChurchData]    Script Date: 02/21/2012 10:44:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[ChurchData](
+	[ChurchCode] [varchar](100) NOT NULL,
+	[LastRun] [datetime] NULL,
+	[Individual] [int] NULL,
+ CONSTRAINT [PK_LastRun] PRIMARY KEY CLUSTERED 
+(
+	[ChurchCode] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+USE [db2050]
+GO
+
+/****** Object:  Table [dbo].[ELMAH_Error]    Script Date: 02/21/2012 10:44:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ELMAH_Error](
+	[ErrorId] [uniqueidentifier] NOT NULL,
+	[Application] [nvarchar](60) NOT NULL,
+	[Host] [nvarchar](50) NOT NULL,
+	[Type] [nvarchar](100) NOT NULL,
+	[Source] [nvarchar](60) NOT NULL,
+	[Message] [nvarchar](500) NOT NULL,
+	[User] [nvarchar](50) NOT NULL,
+	[StatusCode] [int] NOT NULL,
+	[TimeUtc] [datetime] NOT NULL,
+	[Sequence] [int] IDENTITY(1,1) NOT NULL,
+	[AllXml] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_ELMAH_Error] PRIMARY KEY NONCLUSTERED 
+(
+	[ErrorId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[ELMAH_Error] ADD  CONSTRAINT [DF_ELMAH_Error_ErrorId]  DEFAULT (newid()) FOR [ErrorId]
+GO
+
+
+/****** Object:  StoredProcedure [dbo].[ELMAH_GetErrorsXml]    Script Date: 02/21/2012 10:45:19 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[ELMAH_GetErrorsXml]
+(
+    @Application NVARCHAR(60),
+    @PageIndex INT = 0,
+    @PageSize INT = 15,
+    @TotalCount INT OUTPUT
+)
+AS 
+
+    SET NOCOUNT ON
+
+    DECLARE @FirstTimeUTC DATETIME
+    DECLARE @FirstSequence INT
+    DECLARE @StartRow INT
+    DECLARE @StartRowIndex INT
+
+    SELECT 
+        @TotalCount = COUNT(1) 
+    FROM 
+        [ELMAH_Error]
+    WHERE 
+        [Application] = @Application
+
+    -- Get the ID of the first error for the requested page
+
+    SET @StartRowIndex = @PageIndex * @PageSize + 1
+
+    IF @StartRowIndex <= @TotalCount
+    BEGIN
+
+        SET ROWCOUNT @StartRowIndex
+
+        SELECT  
+            @FirstTimeUTC = [TimeUtc],
+            @FirstSequence = [Sequence]
+        FROM 
+            [ELMAH_Error]
+        WHERE   
+            [Application] = @Application
+        ORDER BY 
+            [TimeUtc] DESC, 
+            [Sequence] DESC
+
+    END
+    ELSE
+    BEGIN
+
+        SET @PageSize = 0
+
+    END
+
+    -- Now set the row count to the requested page size and get
+    -- all records below it for the pertaining application.
+
+    SET ROWCOUNT @PageSize
+
+    SELECT 
+        errorId     = [ErrorId], 
+        application = [Application],
+        host        = [Host], 
+        type        = [Type],
+        source      = [Source],
+        message     = [Message],
+        [user]      = [User],
+        statusCode  = [StatusCode], 
+        time        = CONVERT(VARCHAR(50), [TimeUtc], 126) + 'Z'
+    FROM 
+        [ELMAH_Error] error
+    WHERE
+        [Application] = @Application
+    AND
+        [TimeUtc] <= @FirstTimeUTC
+    AND 
+        [Sequence] <= @FirstSequence
+    ORDER BY
+        [TimeUtc] DESC, 
+        [Sequence] DESC
+    FOR
+        XML AUTO
+
+
+GO
+
+/****** Object:  StoredProcedure [dbo].[ELMAH_GetErrorXml]    Script Date: 02/21/2012 10:45:21 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[ELMAH_GetErrorXml]
+(
+    @Application NVARCHAR(60),
+    @ErrorId UNIQUEIDENTIFIER
+)
+AS
+
+    SET NOCOUNT ON
+
+    SELECT 
+        [AllXml]
+    FROM 
+        [ELMAH_Error]
+    WHERE
+        [ErrorId] = @ErrorId
+    AND
+        [Application] = @Application
+
+
+GO
+
+/****** Object:  StoredProcedure [dbo].[ELMAH_LogError]    Script Date: 02/21/2012 10:45:22 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[ELMAH_LogError]
+(
+    @ErrorId UNIQUEIDENTIFIER,
+    @Application NVARCHAR(60),
+    @Host NVARCHAR(30),
+    @Type NVARCHAR(100),
+    @Source NVARCHAR(60),
+    @Message NVARCHAR(500),
+    @User NVARCHAR(50),
+    @AllXml NVARCHAR(MAX),
+    @StatusCode INT,
+    @TimeUtc DATETIME
+)
+AS
+
+    SET NOCOUNT ON
+
+    INSERT
+    INTO
+        [ELMAH_Error]
+        (
+            [ErrorId],
+            [Application],
+            [Host],
+            [Type],
+            [Source],
+            [Message],
+            [User],
+            [AllXml],
+            [StatusCode],
+            [TimeUtc]
+        )
+    VALUES
+        (
+            @ErrorId,
+            @Application,
+            @Host,
+            @Type,
+            @Source,
+            @Message,
+            @User,
+            @AllXml,
+            @StatusCode,
+            @TimeUtc
+        )
+
+
+GO
+
+/****** Object:  StoredProcedure [dbo].[UpdateLastRun]    Script Date: 02/21/2012 10:45:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Kelly Klein
+-- Create date: 5/1/2011
+-- Description:	Insert the last run date.
+-- =============================================
+CREATE PROCEDURE [dbo].[UpdateLastRun]
+	-- Add the parameters for the stored procedure here
+	@churchCode varchar(100)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	Update ChurchData Set LastRun = getDate() Where ChurchCode = @churchCode
+	
+	If @@ROWCOUNT = 0
+		INSERT INTO ChurchData (ChurchCode, LastRun) Values (@churchCode, GETDATE())
+END
+
+GO
+
+/****** Object:  StoredProcedure [dbo].[UpdateTerms]    Script Date: 02/21/2012 10:45:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+-- =============================================
+-- Author:		Kelly Klein
+-- Create date: 5/1/2011
+-- Description:	Insert the last run date.
+-- =============================================
+CREATE PROCEDURE [dbo].[UpdateTerms]
+	-- Add the parameters for the stored procedure here
+	@churchCode varchar(100),
+	@Individual int
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	Update ChurchData Set Individual = @Individual Where ChurchCode = @churchCode
+	
+	If @@ROWCOUNT = 0
+		INSERT INTO ChurchData (ChurchCode, Individual) Values (@churchCode, @Individual)
+END
+
+
+GO
+
+
